13d3861979ecda44c399042d856d6b8e215b11a5
AlexanderSalmanov <alexander@echoes.io>
2023-12-04 18:04:32 +0200

    feat: update home view to track anon/registered users' activity

--- src/main/blueprints/home.py
+++ src/main/blueprints/home.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from flask_login import current_user
+
+from models.users import AnonymousUser
 
 
 bp = Blueprint("home", __name__)
@@ -6,4 +9,18 @@ bp = Blueprint("home", __name__)
 
 @bp.route("/", methods=["GET"])
 def index():
-    return jsonify({"data": "Home Page"})
+    response = {
+        "data": "Home Page"
+    }
+    if current_user.is_authenticated:
+        response["user"] = current_user.email
+        current_user.increment_visits()
+        return jsonify(response), 200
+    ip_address = request.remote_addr
+    anon_user = AnonymousUser.query.filter_by(ip_address=ip_address).first()
+    if not anon_user:
+        anon_user = AnonymousUser.create(ip_address=ip_address)
+    anon_user.increment_visits()
+    response["user"] = anon_user.ip_address
+    return jsonify(response), 200
+
